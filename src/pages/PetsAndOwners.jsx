8dd900e92e4b2b6a7282b79f5dbf58b53b2e19ce
WeeHorse <benjaminberglund@gmail.com>
2026-04-20 01:41:00 +0200

    15 april

--- src/pages/PetsAndOwners.jsx
+++ src/pages/PetsAndOwners.jsx
@@ -1,4 +1,5 @@
-import Lorem from "../parts/Lorem";
+import useFetch from '../utils/useFetch';
+import HeroImage from "../parts/HeroImage";
 
 PetsAndOwners.route = {
   path: '/pets-and-owners',
@@ -7,9 +8,32 @@ PetsAndOwners.route = {
 };
 
 export default function PetsAndOwners() {
-  return <>
-    <h2>Pets & owners</h2>
-    <p>Here will show pets and pet owners.</p>
-    <Lorem paragraphCount={3} />
+
+  const [pets, petOwners, loading] = useFetch(
+    '/api/pets',
+    '/api/petOwners'
+  );
+
+  return !loading && <>
+    <HeroImage
+      src="dog-and-owner.webp"
+      alt="A dog and its owner"
+      heading="Pets & owners"
+    />
+    <p>Here we use our own custom hook to load all data.</p>
+    <h3>Pets</h3>
+    <section className="pets">
+      {pets.map(({ id, name, species }) => <div key={id}>
+        <h4>{name}</h4>
+        <p>{name} is a {species}.</p>
+      </div>)}
+    </section>
+    <h3>Pet owners</h3>
+    <section className="pet-owners">
+      {petOwners.map(({ id, name, email }) => <div key={id}>
+        <h4>{name}</h4>
+        <p>{name} has the email <a href={`mailto:${email}`}>{email}</a>.</p>
+      </div>)}
+    </section>
   </>;
 }
