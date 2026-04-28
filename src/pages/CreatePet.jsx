8710f6aa354231cb943d79be8eb3a4124abedaa1
ReggeMeister <mattiaswaern04@gmail.com>
2026-04-28 14:25:42 +0200

    commit

--- src/pages/CreatePet.jsx
+++ src/pages/CreatePet.jsx
@@ -4,75 +4,70 @@ import useFetch from '../utils/useFetch';
 
 CreatePet.route = {
   path: '/create-pet',
-  label: 'Create a pet',
+  label: 'Create a pet ',
   index: 6
-}
+};
 
 export default function CreatePet() {
 
   const formInitialState = {
     name: '',
     species: '',
-    ownerId: ''
-  }
 
-  const [formData, setFormData] = useState(formInitialState)
-  const [formSent, setFormSent] = useState(false)
-  const navigate = useNavigate()
+    ownerId: '0' // see explanation in sendForm
+  };
+
+  const [formData, setFormData] = useState(formInitialState);
+  const [formSent, setFormSent] = useState(false);
+  const navigate = useNavigate();
 
 
-  const [petOwners, loading] = useFetch('/api/petOwners/')
-  const [unique_species, loading2] = useFetch('/api/unique_species')
-  const [showSpeciesInput, setShowSpeciesInput] = useState(false)
+  console.log(formData);
 
-  if(loading ||loading2) return <p>Loading...</p>
+  const [petOwners, loading] = useFetch('/api/petOwners/');
+  const [unique_species, loading2] = useFetch('/api/unique_species/');
+  const [showSpeciesInput, setShowSpeciesInput] = useState(false);
 
+  if (loading || loading2) return <p>Loading...</p>;
 
   function updateFormData(event) {
-    //console.log(event)
-    const { name: key, value } = event.target;  // event.target.name == "email"
-    // const key = event.target.name
-    // const value = event.target.value
-    // console.log(key, value)
-    // [key] - dynamisk tilldelning av t ex "email" eller "name"
-    setFormData({ ...formData, [key]: value })
+    const { name: key, value } = event.target;
+    setFormData({ ...formData, [key]: value });
   }
 
-
   async function sendForm(event) {
-    event.preventDefault()
-    // console.log(event)
+    event.preventDefault();
     await fetch('/api/pets', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData)
-    })
-    setFormSent(true)
+      body: JSON.stringify(formData,
+        // we are using '0' for ownerId since it works well as a value in the form
+        // but the database/REST-api wants null so we transofrm
+        (key, value) => key === 'ownerId' && value === '0' ? null : value)
+    });
+    setFormSent(true);
   }
 
-
-function setSpecies(event){
-  if(!event.target.value !== "__new__"){
-    setShowSpeciesInput(false)
-    setFormData({species: event.target.value})
-  } else {
-      setShowSpeciesInput(true)
-      setFormData({species: ''})
+  function setSpecies(event) {
+    if (event.target.value !== "__new__") {
+      setShowSpeciesInput(false);
+      setFormData({ ...formData, species: event.target.value });
+    } else {
+      setShowSpeciesInput(true);
+      setFormData({ ...formData, species: '' });
+    }
   }
-}
-
 
   if (formSent) {
-
     return <>
-      <p>The pet owner {formData.name} has been created</p>
+      <p>The pet  {formData.name} has been created</p>
       <button onClick={() => {
         setFormSent(false);
-        setFormData({ ...formInitialState })
-      }}>Create another pet</button>
+        setFormData({ ...formInitialState });
+      }}>Create another pet </button>
       <button onClick={() => navigate('/pets-and-owners')}>
         See the list of pets and their owners</button>
-    </>
+    </>;
 
   } else {
 
@@ -81,29 +76,26 @@ function setSpecies(event){
       <form onSubmit={sendForm}>
         <label>
           Name:
-          <input name="name" type="text" placeholder="Name" value={formData.name} onChange={updateFormData} />
+          <input required name="name" type="text" placeholder="Name" value={formData.name} onChange={updateFormData} />
         </label>
-
-
         <label>
           Species:
           <select name="unique_species" onChange={setSpecies}>
-            <option key="" value="">Select Species</option>
+            <option key="" value="">Select species</option>
             {
               unique_species.map(spec => <option key={spec.species} value={spec.species}>{spec.species}</option>)
             }
-            <option value="__new__">Other (add new species...)</option>
+            <option value="__new__">Other (add new species..)</option>
           </select>
-        {
-          showSpeciesInput &&  <input name="Species" type="text" placeholder="Species" value={formData.species} onChange={updateFormData} />
-        }
-          </label>
-      
+          {
+            showSpeciesInput && <input required name="species" type="text" placeholder="Species" value={formData.species} onChange={updateFormData} />
+          }
+        </label>
 
         <label>
           Owner:
           <select name="ownerId" value={formData.ownerId} onChange={updateFormData}>
-            <option key="0">Select Owner</option>
+            <option key="0" value="0">No owner</option>
             {
               petOwners.map(owner => <option key={owner.id} value={owner.id}>{owner.name}</option>)
             }
@@ -112,7 +104,7 @@ function setSpecies(event){
 
         <button type="submit">Create</button>
       </form>
-    </>
+    </>;
 
   }
 
